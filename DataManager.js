1ab3a197baed0ef3815d512cc3fe9326acd90e98
YF <yfaway@yahoo.com>
2017-08-27 23:08:45 -0400

    First unit tests with mocha and chai.

--- DataManager.js
+++ DataManager.js
@@ -1,21 +1,59 @@
+/**
+ * Manages the main data structure.
+ * @constructor
+ * @param {object} jsonData - the backend data; can be {} for new data.
+ */
 function DataManager(jsonData) {
   var root = jsonData;
   
   if (typeof root.entries === 'undefined') {
     root.entries = [];
+    root.tags = [];
   }
   
+  /**
+   * @return {int} the number of entries
+   */
   this.getEntryCount = function() {
     return root.entries.length;
   }
+
+  /**
+   * @return {array} of tags sorted by most recently used
+   */
+  this.getTags = function() {
+    return root.tags;
+  };
   
+  /**
+   * Adds a new entry.
+   * @param {string} content
+   * @param {array} tags - array of strings
+   * @return {object}
+   */
   this.addEntry = function(content, tags) {
     var entry = { 
       content: content,
       tags: tags,
-      creationDate: new Date().getTime(),
+      creationTime: new Date().getTime(),
     };
                  
     root.entries.push(entry);
+
+    tags.forEach(function(item, index, array) {
+      var idx = root.tags.indexOf(item);
+      if ( idx != -1 ) {
+        var firstElem = root.tags[0];
+        root.tags[0] = item;
+        root.tags[idx] = firstElem;
+      }
+      else {
+        root.tags.push(item);
+      }
+    });
+
+    return entry;
   }
 }
+
+module.exports = DataManager;
---
+++ test/DataManagerTest.js
@@ -0,0 +1,49 @@
+var assert = require('chai').assert;
+//var assert = require('assert');
+var DataManager = require('../DataManager.js');
+
+describe('DataManager', function() {
+  var dm;
+
+  beforeEach(function() {
+    dm = new DataManager({});
+  });
+
+  describe('ctor', function() {
+    it('inits correctly', function() {
+      assert.equal(0, dm.getEntryCount());
+    });
+  });
+
+  describe('addEntry, validParams', function() {
+    it('returns successfully', function() {
+      var content = "testinz";
+      var tags = ['test', 'nada'];
+      var entry = dm.addEntry(content, tags);
+
+      assert.equal(1, dm.getEntryCount());
+      assert.equal(content, entry.content);
+      assert.equal(tags, entry.tags);
+      assert.exists(entry.creationTime);
+      assert.notExists(entry.updateTime);
+
+      assert.deepEqual(tags, dm.getTags());
+    });
+  });
+
+- describe('addEntry, multiple calls', function() {
+    it('maintains unique list of tags sorted by MRU', function() {
+      var content = "testinz";
+      var tagList1 = ['test', 'nada'];
+      dm.addEntry(content, tagList1);
+
+      var tagList2 = ['religion', 'politics', 'test'];
+      dm.addEntry("entry #2", tagList2);
+
+      assert.equal(2, dm.getEntryCount());
+      assert.equal(4, dm.getTags().length);
+      assert.equal('test', dm.getTags()[0]);
+    });
+  });
+
+});
